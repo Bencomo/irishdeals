b48003c3d9ca27b813943799a617f5ffe6660031
Jonathan Bencomo <jonathanbencomo@gmail.com>
2018-02-14 16:38:44 +0000

    settings.py updated 2

--- irishdeals/urls.py
+++ irishdeals/urls.py
@@ -7,7 +7,7 @@ from search import urls as urls_search
 from checkout import urls as urls_checkout
 from products.views import all_products
 from django.views import static
-from .settings import MEDIA_ROOT
+# from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -17,5 +17,5 @@ urlpatterns = [
     url(r'^cart/', include(urls_cart)),
     url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
-    url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
+   # url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
 ]
